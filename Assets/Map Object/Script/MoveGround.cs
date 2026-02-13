73ead78429c2823de4608b7f174865f11452ef80
nkc20236028 <nkc.morishita.haruto@gmail.com>
2026-02-13 11:18:17 +0900

    20260213_1_h

--- Assets/Map Object/Script/MoveGround.cs
+++ Assets/Map Object/Script/MoveGround.cs
@@ -7,15 +7,33 @@ public class MoveGround : MonoBehaviour
     public float moveSpeed = 2f;
 
     Vector3 startPos;
+    Vector3 lastPos;
 
     void Start()
     {
       startPos = transform.position;
+      lastPos = startPos;
+
+      //“®‚«o‚µ‚ğƒ‰ƒ“ƒ_ƒ€‚É
+      if(Random.value < 0.5f)
+        {
+            moveDirection = -moveDirection;
+        }
     }
 
     void Update()
     {
         float t = Mathf.PingPong(Time.time * moveSpeed, moveDistance);
         transform.position = startPos + moveDirection.normalized * t;
+    }
+
+    private void LateUpdate()
+    {
+        lastPos = transform.position;
+    }
+
+    public Vector3 GetDelta()
+    {
+        return transform.position - lastPos;
     }
 }
