d77211f55e4c54ac85812b33f96668c19fb3e235
nkc20236028 <nkc.morishita.haruto@gmail.com>
2026-02-17 14:56:04 +0900

    20260217_2_h

--- Assets/Demo/demo script/DisappearFloor.cs
+++ Assets/Demo/demo script/DisappearFloor.cs
@@ -3,14 +3,26 @@ using System.Collections;
 
 public class DisappearFloor : MonoBehaviour
 {
-    Collider col;
-    Renderer rend;
     bool triggered = false;
+
+    Renderer[] rends;
+    Collider[] cols;
+
+    public float disappearDelay = 0.2f;
+    public float respawnTime = 3f;
+
+    public float shakeTime = 0.5f;
+    public float shakePower = 0.05f;
+    
+
+    Vector3 originalPos;
 
     void Start()
     {
-        col = GetComponent<Collider>();
-        rend = GetComponent<Renderer>();
+        originalPos = transform.parent.position;
+
+        rends = transform.parent.GetComponentsInChildren<Renderer>();
+        cols = transform.parent.GetComponentsInChildren<Collider>();
     }
 
     void OnTriggerEnter(Collider other)
@@ -20,15 +32,26 @@ public class DisappearFloor : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             triggered = true;
-            StartCoroutine(Disappear());
+            StartCoroutine(DisappearAndRespawn());
         }
     }
 
-    IEnumerator Disappear()
+    IEnumerator DisappearAndRespawn()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(disappearDelay);
 
-        col.enabled = false;
-        rend.enabled = false;
+        // è¡Ç∑
+        foreach (Renderer r in rends) r.enabled = false;
+        foreach (Collider c in cols) c.enabled = false;
+
+        // ïúäàë“Çø
+        yield return new WaitForSeconds(respawnTime);
+
+        // ïúäà
+        foreach (Renderer r in rends) r.enabled = true;
+        foreach (Collider c in cols) c.enabled = true;
+
+        triggered = false;
     }
 }
+
